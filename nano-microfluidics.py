22e82e959810fb1634187813c8a7ef5133ef15e7
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2023-12-21 04:34:01 +0530

    Update nano-microfluidics.py

--- nano-microfluidics.py
+++ nano-microfluidics.py
@@ -4,17 +4,24 @@ import math
 pi = math.pi
 
 
-#function to calculate thickness of EDL + particle radius
-#U_p = particle velocity
-#U = fluid velocity
-#miu = dynamic velocity of the fluid
-#epsilon_m = permitivity of the medium
-#Real Part of the Clausius Mossotti (CM) factor (Re_k_w)
-#E_rms = electric field
+#variables
+ni = 5.09338*(10**(-3))
+kappa = 3.995*(10**(-3)) #nm
 
-def doubleLayerThickness(U_p, U, miu, epsilon_m, Re_k_w, E_rms):
-    return (((U_p - U) * 3 * miu)/(epsilon_m * Re_k_w * E_rms))**0.5
+epsilonM = 6.903*(10**(-10))
 
+epsilonL_60 = 7.82*(10**(-10))         #for 60nm
+epsilonL_30 = 8.73*(10**(-10))         #for 30nm
+
+sigmaL_60 = 0.028         #for 60nm
+sigmaL_30 = 0.05         #for 30nm
+
+sigmaM = 5.5*(10**(-6))
+
+zeta_60 = -37.6
+zeta_30 = -33.6
+
+miuI = 6.54*(10**(-8))
 
 #function to calculate thickness of the diffusion layer (kappa inverse)
 #nI = the electrolyte number concentration
@@ -22,8 +29,40 @@ def doubleLayerThickness(U_p, U, miu, epsilon_m, Re_k_w, E_rms):
 #temp = temperature in Kelvin 
 #e = charge of an electron
 #epsilon = dielectric constant
-def kappa(nI, k_b, temp, e, epsilon):
-    return ((2*nI*(e**2))/(epsilon*k_b*temp))**0.5
+def kappaInverse(nI, k_b, temp, e, epsilon):
+    return ((epsilon*k_b*temp)/2*nI*(e**2))**0.5
+
+
+#function to calculate the charge density (sigma star)
+#sigmaStar = charge density
+#phiNote = surface potential
+#epsilon = permitivity of the free space / dielectric constant = zeta potential
+#K = CM factor (kappa)
+def chargeDensity(r1, phiNote, epsilon, K):
+    return ((phiNote * epsilon * (1 + K * r1)) / r1)
+
+
+#function to calculate epsilonL
+def epsilonL(sigmaStar,phiNote,kappaInverse):
+    return (sigmaStar*kappaInverse)/phiNote
+
+
+#function to calculate the general Surface Conductance (Ks)
+#sigmaStar = charge density
+#miuI = ion mobility
+def genSurfaceConductance(sigmaStar,miuI):
+    return sigmaStar * miuI
+
+
+#conductivity of the particle
+#sigmaPCore = conductivity of core particle
+def sigmaP(sigmaPcore, K_s, r2):
+    return sigmaPcore + ((2 * K_s) / r2)
+
+
+#function to calculate epsilonStar
+def epsilonStar(epsilon,sigma, j, w):
+    return epsilon-j*(sigma/w)
 
 
 #function to calculate CM factor k_w
@@ -31,6 +70,13 @@ def kappa(nI, k_b, temp, e, epsilon):
 #epsilonStarM = conplex permitivty of medium
 def k_w(epsilonStarM, epsilonStarP):
     return (epsilonStarP - epsilonStarM)/(epsilonStarP + 2 * epsilonStarM)
+
+
+#epsilon p effective (particle effective)
+def epsilonStarPEff(r2, r1, epsilonStarL, k_w):
+    f1 = (r2/r1)**2
+    f2 = k_w
+    return epsilonStarL*((f1 + 2*f2)/(f1 - f2))
 
 
 #function to calculate time average by Dielectrophoresis forces (F_DEP)
@@ -41,16 +87,58 @@ def timeByF_DEP(pi, epsilonM, a, Re_k_w, E_rms):
     return 2*pi*epsilonM*(a**3)*Re_k_w*(E_rms**2)
 
 
-#function to calculate epsilonStar
-def epsilonStar(epsilon,sigma, j, w):
-    return epsilon-j*(sigma/w)
+#Reynolds number (N_Re)
+#den = density
+#ν = velocity
+#dia = diameter of the particle
+#vis = viscosity
+def Rey_num(den, v, dia, vis)
 
 
-#epsilon p effective (particle effective)
-def epsilonStarPEff(r2, r1, epsilonStarL, k_w):
-    f1 = (r2/r1)**2
-    f2 = k_w
-    return epsilonStarL*((f1 + 2*f2)/(f1 - f2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#function to calculate thickness of EDL + particle radius
+#U_p = particle velocity
+#U = fluid velocity
+#miu = dynamic velocity of the fluid
+#epsilon_m = permitivity of the medium
+#Real Part of the Clausius Mossotti (CM) factor (Re_k_w)
+#E_rms = electric field
+def doubleLayerThickness(U_p, U, miu, epsilonM, Re_k_w, E_rms):
+    return (((U_p - U) * 3 * miu)/(epsilonM * Re_k_w * E_rms))**0.5
+
+
+
+
+
+
+
+
+
 
 
 #r2 = radius of particle + EDL
@@ -59,35 +147,20 @@ delta = 0 #thickness of the double layers
 r2 = r1 + delta
 
 
-#function to calculate the charge density (sigma star)
-#sigmaStar = charge density
-#phiNote = surface potential
-#epsilon = permitivity of the free space / dielectric constant
-#K = CM factor
-def chargeDensity(r1, phiNote, epsilon, K):
-    return ((phiNote * epsilon * (1 + K * r1)) / r1)
-    
-
-#function to calculate epsilonL
-def epsilonL(sigmaStar,phiNote,kappa):
-    return sigmaStar/(phiNote*kappa)
-
-
-#conductivity of the particle
-#sigmaPCore = conductivity of core particle
-def sigmaP(sigmaPcore, K_s, r2):
-    return sigmaPcore + ((2 * K_s) / r2)
-
-
-#function to calculate the general Surface Conductance (Ks)
-#sigmaStar = charge density
-#miuI = ion mobility
-def genSurfaceConductance(sigmaStar,miuI):
-    return sigmaStar * miuI
 
 
 
 
+
+
+
+
+
+
+
+ 
+
+"""
 #graphical representation
 import matplotlib.pyplot as plt
 import numpy as np
@@ -107,3 +180,4 @@ plt.title("x Vs. y")
 plt.plot(myline, mymodel(myline))
 
 plt.show()
+"""
